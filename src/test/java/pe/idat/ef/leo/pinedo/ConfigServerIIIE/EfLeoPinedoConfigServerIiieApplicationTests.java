4c74c80f1a38660d1a10b9ed2f965c3483f2a495
RzoPinedo <rzopinedo@gmail.com>
2022-12-13 23:52:58 -0500

    back

---
+++ src/test/java/pe/idat/ef/leo/pinedo/ConfigServerIIIE/EfLeoPinedoConfigServerIiieApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.ef.leo.pinedo.ConfigServerIIIE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfLeoPinedoConfigServerIiieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
